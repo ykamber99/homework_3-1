87816b63db3bf141e597bb8cbb204afdd0d0bf88
Amber Yu <ky108@duke.edu>
2022-02-22 20:38:42 -0500

    0

---
+++ serve_testhw3.bat
@@ -0,0 +1,4 @@
+cd C:\Users\ky108\PycharmProjects\homework_3-1
+git pull https://%TESTAPP_GIT_PAT%git@github.com:ykamber99/homework_3-1.git
+venv\Scripts\python.exe -m pip install -r requirements.txt
+venv\Scripts\python.exe server.py
